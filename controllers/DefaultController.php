dac24f7b087f874fdbbfe611df8364e79d658490
Chris Stebe <c.stebe@herzogkommunikation.de>
2015-03-12 14:53:47 +0100

    update DefaultController -> add AccessControl behavior (default)

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -2,10 +2,52 @@
 
 namespace dmstr\modules\pages\controllers;
 
-use dmstr\modules\pages\models\Tree;
+use yii\filters\AccessControl;
 
+/**
+ * Class DefaultController
+ * @package dmstr\modules\pages\controllers
+ * @author $Author
+ */
 class DefaultController extends \yii\web\Controller
 {
+    /**
+     * @var boolean whether to enable CSRF validation for the actions in this controller.
+     * CSRF validation is enabled only when both this property and [[Request::enableCsrfValidation]] are true.
+     */
+    public $enableCsrfValidation = false;
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow'   => true,
+                        'actions' => ['index', 'view', 'create', 'update', 'delete'],
+                        'roles'   => ['@']
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function beforeAction($action)
+    {
+        if (parent::beforeAction($action)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
