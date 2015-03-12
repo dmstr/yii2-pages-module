866bdfaeab260b76d5ae590ca2f1e65a6a8058a1
Chris Stebe <c.stebe@herzogkommunikation.de>
2015-03-12 14:54:01 +0100

    conde formatting

--- views/default/index.php
+++ views/default/index.php
@@ -6,13 +6,15 @@ use dmstr\modules\pages\models\Tree;
 
 echo "<h1>Pages</h1>";
 
-echo TreeView::widget([
-    // single query fetch to render the tree
-    'query' => Tree::find()->addOrderBy('root, lft'),
-    'headingOptions' => ['label' => 'Categories'],
-    'fontAwesome' => true,     // optional
-    'isAdmin' => true,         // optional (toggle to enable admin mode)
-    'displayValue' => 1,        // initial display value
-    //'softDelete' => true,    // normally not needed to change
-    //'cacheSettings' => ['enableCache' => true] // normally not needed to change
-]);
+echo TreeView::widget(
+    [
+        // single query fetch to render the tree
+        'query'          => Tree::find()->addOrderBy('root, lft'),
+        'headingOptions' => ['label' => 'Categories'],
+        'fontAwesome'    => true,     // optional
+        'isAdmin'        => true,         // optional (toggle to enable admin mode)
+        'displayValue'   => 1,        // initial display value
+        //'softDelete' => true,    // normally not needed to change
+        //'cacheSettings' => ['enableCache' => true] // normally not needed to change
+    ]
+);
