c99105d984bbfa26a4004d113db9881a3e869bb8
Chris Stebe <c.stebe@herzogkommunikation.de>
2015-03-12 23:33:03 +0100

    add getMenuItems from nested set rootNode (TBF -> url creation)

--- views/default/index.php
+++ views/default/index.php
@@ -14,7 +14,65 @@ echo TreeView::widget(
         'fontAwesome'    => true,     // optional
         'isAdmin'        => true,         // optional (toggle to enable admin mode)
         'displayValue'   => 1,        // initial display value
-        //'softDelete' => true,    // normally not needed to change
+        'softDelete'     => true,    // normally not needed to change
         //'cacheSettings' => ['enableCache' => true] // normally not needed to change
     ]
 );
+
+/**
+ * Playground for generating structured menuItems array
+ */
+function getMenuItems($rootNode)
+{
+    // tree mapping
+    $treeMap = [];
+    $stack = [];
+
+    if (count($rootNode) > 0) {
+
+        foreach ($rootNode as $node) {
+
+            // prepare node identifiers
+            $nodeOptions  = [
+                'data-pageId' => $node->id,
+                'data-lvl' => $node->lvl,
+            ];
+
+            $itemTemplate  = [
+                'label'       => $node->name,
+                'url'         => '',// TODO $node->createUrl(),
+                'active'      => $node->active,
+                'itemOptions' => $nodeOptions,
+            ];
+            $item          = $itemTemplate;
+            $item['items'] = null;
+
+            // Count items in stack
+            $counter = count($stack);
+
+            // Check on different levels
+            while ($counter > 0 && $stack[$counter - 1]['itemOptions']['data-lvl'] >= $item['itemOptions']['data-lvl']) {
+                array_pop($stack);
+                $counter--;
+            }
+
+            // Stack is now empty (check root again)
+            if ($counter == 0) {
+                // assign root node
+                $i           = count($treeMap);
+                $treeMap[$i] = $item;
+                $stack[]     = &$treeMap[$i];
+            } else {
+                // add the node to parent node
+                $i                                = count($stack[$counter - 1]['items']);
+                $stack[$counter - 1]['items'][$i] = $item;
+                $stack[]                          = &$stack[$counter - 1]['items'][$i];
+            }
+        }
+    }
+    return array_filter($treeMap);
+}
+
+$rootNode = Tree::find()->addOrderBy('root, lft')->all();
+\yii\helpers\VarDumper::dump(getMenuItems($rootNode), 10, true);
+
