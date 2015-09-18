e6dc99d943fd79625a2e3841c61d2f6bcecf81e5
Tobias Munk <schmunk@usrbin.de>
2015-09-18 03:12:33 +0200

    added auth item migration

---
+++ migrations/m150918_031100_auth_items.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\db\Migration;
+
+class m150918_031100_auth_items extends Migration
+{
+	public function up()
+	{
+		$auth = Yii::$app->authManager;
+
+		if ($auth) {
+			$permission = $auth->createPermission('pages_default_page');
+			$permission->description = 'CMS-Page Action';
+			$auth->add($permission);
+		}
+	}
+
+	public function down()
+	{
+		echo "m150623_164544_auth_items cannot be reverted.\n";
+
+		return false;
+	}
+}
