ac6d2f281d633a1557dc531c2bdfb9cfe0238b54
Tobias Munk <schmunk@usrbin.de>
2015-11-24 10:07:28 +0100

    updated headline, css-classes

--- views/default/index.php
+++ views/default/index.php
@@ -1,17 +1,17 @@
 <?php
 /* @var $this yii\web\View */
 
+use dmstr\modules\pages\models\Tree;
 use kartik\tree\TreeView;
 use kartik\tree\TreeViewInput;
-use dmstr\modules\pages\models\Tree;
-use\yii\helpers\Inflector;
+use yii\helpers\Inflector;
 
 $title = Inflector::titleize($this->context->module->id);
 \dmstr\modules\pages\assets\PagesAsset::register($this);
 
 ?>
 
-<h2>Pages</h2>
+    <h1>Pages</h1>
 
 <?php
 /**
@@ -33,24 +33,28 @@ HTML;
 $mainTemplate = <<< HTML
 <div class="row">
     <div class="col-md-4" id="pages-detail-wrapper">
+        <div class="box boy-body">
         {wrapper}
+        </div>
     </div>
     <div class="col-md-8" id="pages-detail-panel">
+    <div class="box box-body">
         {detail}
+        <div>
     </div>
 </div>
 HTML;
 
 echo TreeView::widget(
     [
-        'query'           => Tree::find()->addOrderBy('root, lft')->andWhere([Tree::ATTR_ACCESS_DOMAIN => \Yii::$app->language]),
-        'isAdmin'         => true,
-        'softDelete'      => false,
-        'displayValue'    => 1,
+        'query' => Tree::find()->addOrderBy('root, lft')->andWhere([Tree::ATTR_ACCESS_DOMAIN => \Yii::$app->language]),
+        'isAdmin' => true,
+        'softDelete' => false,
+        'displayValue' => 1,
         'wrapperTemplate' => "{header}{footer}{tree}",
-        'headingOptions'  => ['label' => $title . '-Module'],
-        'treeOptions'     => ['style' => 'height:600px'],
-        'headerTemplate'  => $headerTemplate,
-        'mainTemplate'    => $mainTemplate,
+        'headingOptions' => ['label' => $title . '-Module'],
+        'treeOptions' => ['style' => 'height:600px'],
+        'headerTemplate' => $headerTemplate,
+        'mainTemplate' => $mainTemplate,
     ]
 );
