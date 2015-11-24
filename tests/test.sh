6050576aafa3d8dc00b09c197d684151398c9a21
Tobias Munk <schmunk@usrbin.de>
2015-11-24 09:40:08 +0100

    updated testing output

--- tests/test.sh
+++ tests/test.sh
@@ -2,9 +2,11 @@
 
 export COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME:-testpages}
 
-docker-compose --x-networking pull
+#docker-compose --x-networking pull
 
 docker-compose --x-networking up -d & wait
+docker-compose --x-networking ps
+
 docker-compose --x-networking run php sh /app/src/init.sh
 
 docker-compose --x-networking run php codecept run -c /app/vendor/dmstr/yii2-pages-module/codeception.yml
