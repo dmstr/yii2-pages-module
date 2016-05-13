6f6190d93901d59eb52c43d778624c8af289a3a0
Alexander <akaSStalkALEX@gmail.com>
2016-05-13 12:46:16 +0500

    Update migration for Postgres support

--- migrations/m160411_082658_rename_name_id_column.php
+++ migrations/m160411_082658_rename_name_id_column.php
@@ -8,10 +8,17 @@ class m160411_082658_rename_name_id_column extends Migration
     {
         $this->dropIndex('name_id_UNIQUE', 'dmstr_page');
         $this->renameColumn('dmstr_page', 'name_id', 'domain_id');
-        $this->execute('ALTER TABLE dmstr_page MODIFY COLUMN domain_id VARCHAR(255) NOT NULL;');
-        $this->execute(
-            'ALTER TABLE `dmstr_page` ADD UNIQUE INDEX `name_id_UNIQUE` (`domain_id`, `access_domain`);'
-        );
+        if ($this->db->driverName === 'pgsql') {
+            $this->execute('ALTER TABLE dmstr_page ALTER COLUMN domain_id SET NOT NULL;');
+            $this->execute(
+                'CREATE UNIQUE INDEX name_id_UNIQUE ON dmstr_page (domain_id, access_domain);'
+            );            
+        } else {
+            $this->execute('ALTER TABLE dmstr_page MODIFY COLUMN domain_id VARCHAR(255) NOT NULL;');
+            $this->execute(
+                'ALTER TABLE `dmstr_page` ADD UNIQUE INDEX `name_id_UNIQUE` (`domain_id`, `access_domain`);'
+            );
+        }
     }
 
     public function down()
