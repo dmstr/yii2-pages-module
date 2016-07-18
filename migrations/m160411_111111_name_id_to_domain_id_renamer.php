ae024d83b89ea0c9a5ca1cf33194ac552b164aed
Tobias Munk <schmunk@usrbin.de>
2016-07-18 22:45:31 +0200

    updated tests, using docker-yii2-app

--- migrations/m160411_111111_name_id_to_domain_id_renamer.php
+++ migrations/m160411_111111_name_id_to_domain_id_renamer.php
@@ -8,19 +8,15 @@ class m160411_111111_name_id_to_domain_id_renamer extends Migration
     {
         $languages = explode(',', getenv('APP_LANGUAGES'));
 
-        if (count($languages) > 0 && $languages[0] !== '') {
-            foreach ($languages as $language) {
-                $this->execute(
-                    "
+        foreach ($languages as $language) {
+            $this->execute(
+                "
                 UPDATE dmstr_page SET domain_id = REPLACE (domain_id, '_$language', '') WHERE domain_id LIKE '%_$language';
                 "
-                );
-            }
-
-            return true;
+            );
         }
 
-        return false;
+        return true;
     }
 
     public function down()
