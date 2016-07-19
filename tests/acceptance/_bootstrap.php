408afec179c7e8a42a1fbb0158524663d8c4a540
Tobias Munk <schmunk@usrbin.de>
2016-07-19 05:10:48 +0200

    fixed test config

--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -4,7 +4,6 @@
  */
 $config = yii\helpers\ArrayHelper::merge(
     require('/app/src/config/main.php'),
-    require(__DIR__ . '/../_config/config.php'),
     [
         'controllerNamespace' => 'app\controllers',
     ]
