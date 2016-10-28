dd29850a28a6d6efde75fcaf9ec87c49646de754
Tobias Munk <schmunk@usrbin.de>
2016-10-28 03:56:49 +0200

    added settings migration

---
+++ migrations/m161029_011345_settings.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+class m161029_011345_settings extends Migration
+{
+    public function up()
+    {
+        if (Yii::$app->has('settings')) {
+            if (!Yii::$app->settings->get('availableRoutes', 'pages', false)) {
+                Yii::$app->settings->set('availableRoutes', '/pages/default/page', 'pages', 'string');
+            }
+            if (!Yii::$app->settings->get('availableGlobalRoutes', 'pages', false)) {
+                Yii::$app->settings->set('availableGlobalRoutes', '/pages/default', 'pages', 'string');
+            }
+            if (!Yii::$app->settings->get('availableViews', 'pages', false)) {
+                Yii::$app->settings->set('availableViews',
+                    '@vendor/dmstr/yii2-prototype-module/src/views/render/twig.php',
+                    'pages', 'string');
+            }
+        }
+        return true;
+    }
+
+    public function down()
+    {
+        if (Yii::$app->has('settings')) {
+            Yii::$app->settings->delete('availableRoutes', 'pages');
+            Yii::$app->settings->delete('availableGlobalRoutes', 'pages');
+            Yii::$app->settings->delete('availableViews', 'pages');
+        }
+        return true;
+    }
+
+}
