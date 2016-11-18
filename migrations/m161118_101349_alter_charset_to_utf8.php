253b8ca8c1e528211674f82b4c5341fef8aa3232
Tobias Munk <schmunk@usrbin.de>
2016-11-18 13:41:06 +0100

    added migration to convert table to utf8

---
+++ migrations/m161118_101349_alter_charset_to_utf8.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Migration;
+
+class m161118_101349_alter_charset_to_utf8 extends Migration
+{
+    public function up()
+    {
+        Yii::$app->db->createCommand("ALTER TABLE dmstr_page CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci ;")->execute();
+    }
+
+    public function down()
+    {
+        echo "m160721_101347_alter_charset_to_utf8 cannot be reverted.\n";
+
+        return false;
+    }
+}
