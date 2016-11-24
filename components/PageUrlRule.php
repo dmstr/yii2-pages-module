137320cb64261815235b889334cd591f79878d1f
Christopher Stebe <c.stebe@herzogkommunikation.de>
2016-11-24 17:59:54 +0100

    fixed PageUrlRule class, respect all request params

--- components/PageUrlRule.php
+++ components/PageUrlRule.php
@@ -1,10 +1,4 @@
 <?php
-
-namespace dmstr\modules\pages\components;
-
-use yii\web\UrlRuleInterface;
-use yii\base\Object;
-
 /**
  * @link http://www.diemeisterei.de/
  * @copyright Copyright (c) 2016 diemeisterei GmbH, Stuttgart
@@ -12,21 +6,74 @@ use yii\base\Object;
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+namespace dmstr\modules\pages\components;
+
+use dmstr\modules\pages\models\Tree;
+use yii\web\UrlRuleInterface;
+use yii\base\Object;
+
+/**
+ * Class PageUrlRule
+ * @package dmstr\modules\pages\components
+ * @author Christopher Stebe <c.stebe@herzogkommunikation.de>
+ */
 class PageUrlRule extends Object implements UrlRuleInterface
 {
 
+    /**
+     * @param \yii\web\UrlManager $manager
+     * @param string $route
+     * @param array $params
+     *
+     * @return bool|string
+     */
     public function createUrl($manager, $route, $params)
     {
-        if ($route === 'pages/default/page' && isset($params['pageId']) && isset($params['pageSlug'])) {
+        if ($route === ltrim(Tree::DEFAULT_PAGE_ROUTE, '/')) {
 
-            $pagePath = (isset($params['pagePath']))?$params['pagePath'].'/':'';
+            /**
+             * Build page url
+             */
+            $pageId = '';
+            if (isset($params['pageId'])) {
+                $pageId = '-' . $params['pageId'];
+                unset($params['pageId']);
+            }
 
-            return 'p/'.$pagePath.$params['pageSlug'].'-'.$params['pageId'].'.html';
+            $pageSlug = '';
+            if (isset($params['pageSlug'])) {
+                $pageSlug = $params['pageSlug'];
+                unset($params['pageSlug']);
+            }
+
+            $pagePath = '';
+            if (isset($params['pagePath'])) {
+                $pagePath = $params['pagePath'] . '/';
+                unset($params['pagePath']);
+            }
+
+            $pageUrl = $pagePath . $pageSlug . $pageId;
+
+            /**
+             * Add additional request params if set
+             */
+            if (!empty($params) && ($query = http_build_query($params)) !== '') {
+                $pageUrl .= '?' . $query;
+            }
+
+            return $pageUrl;
         }
 
         return false;  // this rule does not apply
     }
 
+    /**
+     * @param \yii\web\UrlManager $manager
+     * @param \yii\web\Request $request
+     *
+     * @return bool
+     */
     public function parseRequest($manager, $request)
     {
         return false;  // this rule does not apply
