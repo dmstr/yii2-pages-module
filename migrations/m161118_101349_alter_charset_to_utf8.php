a39a6f90a9a39a386e153e1c6804b28760cc1f43
Christopher Stebe <c.stebe@herzogkommunikation.de>
2017-02-01 10:00:46 +0100

    updated db charset migration, use inclusion instead of exclusion

--- migrations/m161118_101349_alter_charset_to_utf8.php
+++ migrations/m161118_101349_alter_charset_to_utf8.php
@@ -6,7 +6,7 @@ class m161118_101349_alter_charset_to_utf8 extends Migration
 {
     public function up()
     {
-        if ($this->db->driverName !== 'pgsql') {
+        if ($this->db->driverName === 'mysql') {
             Yii::$app->db->createCommand("ALTER TABLE dmstr_page CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci ;")->execute();
         }
     }
