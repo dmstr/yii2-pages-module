841995c3581c7bee1fceb26ae581e890eecf26cc
Tobias Munk <schmunk@usrbin.de>
2017-04-03 15:58:58 +0200

    updated migrations to support postgres

--- migrations/m170322_204909_update_timestamp_columns.php
+++ migrations/m170322_204909_update_timestamp_columns.php
@@ -7,8 +7,8 @@ class m170322_204909_update_timestamp_columns extends Migration
 {
     public function up()
     {
-        $this->alterColumn('dmstr_page', 'created_at', 'DATETIME NULL');
-        $this->alterColumn('dmstr_page', 'updated_at', 'DATETIME NULL');
+        $this->alterColumn('dmstr_page', 'created_at', $this->dateTime());
+        $this->alterColumn('dmstr_page', 'updated_at', $this->dateTime());
     }
 
     public function down()
