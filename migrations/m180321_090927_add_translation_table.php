b48bfc93cb29920e399916eafc79d67899260f8b
Elias Luhr <e.luhr@herzogkommunikation.de>
2018-03-26 10:27:58 +0200

    updated foreign keys, disabled migrate down

--- migrations/m180321_090927_add_translation_table.php
+++ migrations/m180321_090927_add_translation_table.php
@@ -28,7 +28,7 @@ class m180321_090927_add_translation_table extends Migration
             ],'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB'
         );
 
-        $this->addForeignKey('FK_page_translation_page', 'dmstr_page_translation','page_id','dmstr_page','id','CASCADE','CASCADE');
+        $this->addForeignKey('FK_page_translation_page', 'dmstr_page_translation','page_id','dmstr_page','id');
 
         // select all pages to insert them into the translation table
         $query = new \yii\db\Query();
@@ -66,38 +66,8 @@ class m180321_090927_add_translation_table extends Migration
      */
     public function safeDown()
     {
-
-        $this->addColumn('dmstr_page','name','VARCHAR(60) NOT NULL AFTER id');
-        $this->addColumn('dmstr_page','page_title','VARCHAR(255) NULL AFTER name');
-        $this->addColumn('dmstr_page','default_meta_keywords','VARCHAR(255) NULL AFTER page_title');
-        $this->addColumn('dmstr_page','default_meta_description','TEXT(2048) NULL AFTER default_meta_keywords');
-
-        // transfer translations back into pages table
-        $query = new \yii\db\Query();
-        $pages = $query->select([
-            'page_id',
-            'language',
-            'name',
-            'page_title',
-            'default_meta_keywords',
-            'default_meta_description',
-            'SUM(1)'
-        ])->from('dmstr_page_translation')->groupBy('page_id')->all();
-
-        foreach ($pages as $page) {
-            // check if sum of pages grouped by page id is equal to sum of configured languages. If so set it to global
-            $language = (int)$page['SUM(1)'] === count(\Yii::$app->urlManager->languages) ? '*' : $page['language'];
-
-            $this->update('dmstr_page', [
-                'name' => $page['name'],
-                'page_title' => $page['page_title'],
-                'default_meta_keywords' => $page['default_meta_keywords'],
-                'default_meta_description' => $page['default_meta_description'],
-            ], 'id = ' . $page['page_id'] . ' AND access_domain = "' . $language . '"');
-        }
-
-        $this->dropForeignKey('FK_page_translation_page', 'dmstr_page_translation');
-        $this->dropTable('dmstr_page_translation');
+        echo "m180321_090927_add_translation_table cannot be reverted.\n";
+        return false;
     }
     
     private function insertPageTranslation($page, $language) {
