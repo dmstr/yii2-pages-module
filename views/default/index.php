196d094e7f07c2ce83f1bee5e06decdda92b0613
Elias Luhr <e.luhr@herzogkommunikation.de>
2018-10-10 15:41:07 +0200

    removed route true from check. Otherwise copy will be allowed to everyone who access pages

--- views/default/index.php
+++ views/default/index.php
@@ -33,7 +33,7 @@ $toolbar = [];
 /**
  * Links to settings and copy pages area for toolbar
  */
-if (\Yii::$app->user->can(Tree::COPY_ACCESS_PERMISSION, ['route' => true])) {
+if (\Yii::$app->user->can(Tree::COPY_ACCESS_PERMISSION)) {
     $copyPages = [
         'icon'    => 'copy',
         'url'     => ['/pages/copy'],
