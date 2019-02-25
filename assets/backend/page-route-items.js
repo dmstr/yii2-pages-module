a1a85757399e7bd309036407de8e919a5a27de4a
Elias Luhr <e.luhr@herzogkommunikation.de>
2019-02-25 12:46:02 +0100

    update page-route-item.js syntax

--- assets/backend/page-route-items.js
+++ assets/backend/page-route-items.js
@@ -14,8 +14,9 @@ $(function () {
         var schema = resp.schema;
         if (schema !== undefined && schema !== false) {
           var jsonEditorList = window.jsonEditors;
-          var editorId = self.data('editor-id');
-          var editor = jsonEditorList.find(editor => editor.element.id === "tree-request_params-container");
+          var editor = jsonEditorList.find(function (editor) {
+            return editor.element.id === "tree-request_params-container";
+          });
 
           if (editor !== undefined) {
             var element = editor.element;
