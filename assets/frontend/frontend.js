7550d7b754c1f96418846e6a26b878a69e608168
Marc Mautz <m.mautz@herzogkommunikation.de>
2020-05-19 14:56:10 +0200

    fixed event handling

--- assets/frontend/frontend.js
+++ assets/frontend/frontend.js
@@ -1,10 +1,10 @@
-$(document).on('ready cookieUpdate',function () {
-
-    // TODO: check value
+function toggleCellOutline () {
     if (typeof $.cookie === 'function' && $.cookie('app-frontend-view-mode')) {
         $('.dmstr-pages-invisible-frontend').hide();
     } else {
         $('.dmstr-pages-invisible-frontend').show();
     }
+}
 
-});
+$(document).ready(toggleCellOutline);
+$(document).on('cookieUpdate', toggleCellOutline);
